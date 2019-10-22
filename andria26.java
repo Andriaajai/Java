6d8945bccbf8836cdb3c5d0d0cd0b10433e3e616
amalaabraham <36256165+amalaabraham@users.noreply.github.com>
2019-10-22 11:56:00 +0530

    Update andria26.java

--- andria26.java
+++ andria26.java
@@ -11,6 +11,11 @@ public andria26(String m)
 setLayout(new FlowLayout());
 setSize(300,200);
 setVisible(true);
+setLocation(200,50);
+setResizable(false);
+setTitle(m);
+t1=new TextField(20);
+add(t1);
 t1.setText("201");
 t2=new TextField(20);
 add(t2);
@@ -20,7 +25,10 @@ add(t3);
 String s1=t1.getText();
 String s2=t2.getText();
 int x,y,z;
+x=Integer.parseInt(s1);
 y=Integer.parseInt(s2);
+z=x+y;
 m=String.valueOf(z);
+t3.setText(m);
 }
 }
