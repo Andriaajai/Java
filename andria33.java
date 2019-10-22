507da3993adeab4ee35237a6373cf6e708deb787
Andriaajai <35872115+Andriaajai@users.noreply.github.com>
2019-10-22 12:27:57 +0530

    Update andria33.java

--- andria33.java
+++ andria33.java
@@ -1,21 +1,15 @@
 import java.awt.*;
 import java.applet.*;
-public class andria33 extends Applet
+class andria33 extends Applet
 {
 String s;
 Image i;
-public void init()
+void init()
 {
-s="java is interesting";
 i=getImage(getCodeBase(),"img18.jpg");
 }
-public void paint(Graphics g)
+void paint(Graphics g)
 {
 g.drawImage(i,20,20,this);
-Font f=new Font("Georgia",Font.ITALIC,30);
-g.setFont(f);
-g.setColor(Color.white);
-//setBackground(Color.red);
-g.drawString(s,200,200);
 }
 }
